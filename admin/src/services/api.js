f330cd70dcd60213b337a8a77919d2a3c7ecf1af
Cicero, Feijo <cicero.feijo@gmail.com>
2023-02-23 16:44:19 -0300

    Add API calls with Bearer

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // URL da API
-const baseURL = 'http://localhost:3033'
+//const baseURL = 'http://localhost:3033'
+const baseURL = 'https://1606024-nodejs.vercel.app'
 
 const api = axios.create({
   baseURL
--- admin/src/services/departamentos.js
+++ admin/src/services/departamentos.js
@@ -1,7 +1,11 @@
 import api from './api'
 
+const headers = {
+  'Authorization': 'Bearer API-1606024'
+}
+
 const getDepartamentos = async () => {
-  const resp = await api.get('/departamentos')
+  const resp = await api.get('/departamentos', { headers })
 
   return resp
 }
